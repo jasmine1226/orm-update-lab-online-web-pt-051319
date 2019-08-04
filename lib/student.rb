1927d7bc016cf6ad31e8d0c4fd19c2ea9e9fb3ec
Jasmine Chen <jasmine1226@gmail.com>
2019-08-04 23:08:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
 
   def self.new_from_db(row)
     puts "#{row}"
-    student = self.new(row[1],row[2])
+    student = self.new(row[1],row[2],row[0])
     puts student.name
     puts student.grade
     student
