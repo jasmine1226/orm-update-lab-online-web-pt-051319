79e25e9813ea2146369c17d22f1ec8186dbf93ed
Jasmine Chen <jasmine1226@gmail.com>
2019-08-04 22:45:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,11 @@ class Student
   end
 
   def save
-    self
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (? , ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self,grade)
   end
 
 
