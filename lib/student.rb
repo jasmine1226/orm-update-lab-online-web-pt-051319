d473d7a959be27a0ea5bec56cf890e3552605a1e
Jasmine Chen <jasmine1226@gmail.com>
2019-08-04 22:53:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,10 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
+      LIMIT 1
     SQL
-    DB[:conn].execute(sql,name)
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
